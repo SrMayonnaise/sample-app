9d2061d352f327ad254e239895441f230310c189
Franco <fran.vasquezn@duocuc.cl>
2023-05-23 05:24:56 +0000

    cambiado el script para evitar errores

--- sample-app.sh
+++ sample-app.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -rf tempdir
 mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
@@ -10,13 +11,21 @@ cp -r static/* tempdir/static/.
 
 echo "FROM python" >> tempdir/Dockerfile
 echo "RUN pip install flask" >> tempdir/Dockerfile
-echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
-echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
-echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 5050" >> tempdir/Dockerfile
 echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
 cd tempdir
+
+# Verifica si Docker está instalado y disponible en el entorno de Jenkins
+if ! [ -x "$(command -v docker)" ]; then
+  echo 'Error: Docker no está instalado o no se encuentra en la ruta adecuada.' >&2
+  exit 1
+fi
+
 docker build -t jenki .
 docker run -t -d -p 5050:5050 --name jenkimg jenki
-docker ps -a 
+docker ps -a
+
